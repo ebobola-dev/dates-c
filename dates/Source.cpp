a6b0a12a0484e619ee502134300737b180a48076
ebobola-web <romawot94@gmail.com>
2021-12-07 13:13:56 +0300

    Добавьте файлы проекта.

---
+++ dates/Source.cpp
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <locale.h>
+#include <Windows.h>
+
+typedef struct {
+	int day;
+	int month;
+	int year;
+	int isLeap;
+	int maxDays;
+	int BC;
+} date;
+
+int isLeapYear(int);
+int maxDaysInMonth(int, int);
+int dateIsCorrect(int, int, int);
+void printDate(date);
+date addDays(date, int);
+
+const int maxDaysArr[12] = {
+	31, 28, 31, 30,
+	31, 30, 31, 31,
+	30, 31, 30, 31
+};	
+
+int main() {
+	setlocale(LC_ALL, "Rus");
+	int cont = 1, trash = 0, enterCount = 0, addedDays;
+	int tempDate[3];
+
+	while (cont) {
+		tempDate[0] = 0;
+		tempDate[1] = 0;
+		tempDate[2] = 0;
+		// Ввод даты
+		do {
+			printf("Введите дату(Пример: 12 12 2020): ");
+			enterCount = scanf_s("%d%d%d", &tempDate[0], &tempDate[1], &tempDate[2]);
+			while (trash = getchar() != '\n');
+			if (enterCount < 3) {
+				system("cls");
+				printf("Вводите только числа\n");
+			}
+		} while (enterCount < 3);
+
+		while (!dateIsCorrect(tempDate[0], tempDate[1], tempDate[2])) {
+			system("cls");
+			printf("Некорректная дата\nПопробуйте заново\n");
+			do {
+				printf("Введите дату(Пример: 12 12 2020): ");
+				enterCount = scanf_s("%d%d%d", &tempDate[0], &tempDate[1], &tempDate[2]);
+				while (trash = getchar() != '\n');
+				if (enterCount < 3) printf("Вводите только числа\n");
+			} while (enterCount < 3);
+		}
+
+		date userDate = {
+			.day = tempDate[0],
+			.month = tempDate[1],
+			.year = tempDate[2] > 0 ? tempDate[2] : -tempDate[2],
+			.isLeap = isLeapYear(tempDate[2]),
+			.maxDays = maxDaysInMonth(tempDate[1], tempDate[2]),
+			.BC = tempDate[2] < 0 ? 1 : 0,
+		};
+
+		printf("Вы ввели дату: ");
+		printDate(userDate);
+		printf("\n");
+		
+		// Ввод прибавляемых дней
+		do {
+			printf("Введите кол-во дней, которые нужно прибавить: ");
+			enterCount = scanf_s("%d", &addedDays);
+			while (trash = getchar() != '\n');
+			if (enterCount < 1) {
+				printf("Вводите только числа\n");
+			}
+		} while (enterCount < 1);
+
+		userDate = addDays(userDate, addedDays);
+		printf("Новая дата: ");
+		printDate(userDate);
+		printf("\n");
+
+
+		printf("\n");
+	}
+	return 0;
+}
+
+int isLeapYear(int year) {
+	if (year % 100 == 0 && year % 400 != 0) return 0;
+	if (year % 4 == 0) return 1;
+	return 0;
+}
+
+int maxDaysInMonth(int month, int year) {
+	return isLeapYear(year) ? (month == 2 ? 29 : maxDaysArr[month - 1]) : maxDaysArr[month - 1];
+}
+
+int dateIsCorrect(int days, int month, int year) {
+	if (days < 1 || month < 1 || month > 12 || year == 0) return 0;
+	int maxDays = maxDaysInMonth(month, year);
+	if (days > maxDays) return 0;
+	return 1;
+}
+
+void printDate(date _date) {
+	printf("%d.%d.%d", _date.day, _date.month, _date.year);
+	if (_date.BC) printf(" до н.э");
+}
+
+date addDays(date _date, int addedDays) {
+	for (int i = 0; i < addedDays; i++) {
+		// Если пора прибавлять месяцы
+		if (_date.day + 1 > _date.maxDays) {
+			// Если пора прибавлять года
+			if (_date.month + 1 > 12) {
+				_date.month = 1;
+				_date.year++;
+				_date.isLeap = isLeapYear(_date.year); // Это просто для красоты
+			}
+			else {
+				_date.month++;
+				_date.maxDays = maxDaysInMonth(_date.month, _date.year);
+			}
+			_date.day = 1;
+		}
+		else _date.day++;
+	}
+	return _date;
+}
